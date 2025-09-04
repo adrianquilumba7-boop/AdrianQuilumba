16eb49b497dc9eb95c5e651df4b448e20e4ae118
adrianquilumba7-boop <adrianquilumba7@gmail.com>
2025-09-04 10:38:16 -0500

    iniciar sesion
    sb

--- tasks/views.py
+++ tasks/views.py
@@ -2,17 +2,41 @@
 
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
+
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+def signout(request):
+     logout(request)
+     return redirect("home")
 
 def tasks(request):
     return render(request, "tasks.html")
+
+def signin(request):
+     if request.method == "GET":
+          return render(request,
+                        "signin.html",
+                        {"form":AuthenticationForm()})
+     else:
+        user = authenticate(request,
+                             username=request.POST.get('username'),
+                             password=request.POST.get('password'))
+        
+        if user is None:
+             return render(request,
+                              "signin.html",
+                              {"form":AuthenticationForm(),
+                              "error":"Usuario o contraseña incorrecta"})
+        else:
+                 login(request, user)
+                 return redirect("tasks")
+                
 
 def signup(request):
     if request.method == "GET":
@@ -38,4 +62,4 @@ def signup(request):
                  return render(request,
                               'signup.html',
                               {"form": UserCreationForm(),
-                               "error":"Error, Las contraseñas no coinciden"})
+                               "error":"Error Las contraseñas no coinciden"})
